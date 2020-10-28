29345257eab3e2a0aeacf8f4d6c663eb475476ee
prayag khurana <prayagkhurana999@gmail.com>
2020-10-28 21:37:41 +0530

    speech_recognizer

---
+++ speech_recognizer.py
@@ -0,0 +1,10 @@
+#speech recognition it will recognise our voice and converted to text
+
+import speech_recognition os sp
+x=sp.Recogniser()  #instance
+with sp.Microphone() as source:  #it us equalent to sp.Microphone bt after its use it will continue to work
+    audio=x.listen(source)  #here recogniser is saying listen from source
+    text=x.recognize_google(audio)
+    print(text)
+    
+#also have to install PyAudio  otherwise it will thorugh error
